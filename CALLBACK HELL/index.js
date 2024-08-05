ef3cbdf6324e1b0c765be5abf26042aa09e31f21
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-05 21:21:27 +0530

    ep01

--- CALLBACK HELL/index.js
+++ CALLBACK HELL/index.js
@@ -1,19 +1,14 @@
+
+
 const cart = ["shoes","pants","kurta"];
 
-//create order
 api.createOrder(cart, function () {
-//proceed to payment
-    api.proceedToPayment(function() {
-        //ordersummary
-        api.showOrderSummary(function() {
-            //updatewallet
-        api.updateWallet()
-       })
+
+    api.proceedToPayment()
+
     })
-})
 
 
-//The above structure is known as the Pyramid of doom
 
 
 
